163f942aa37157f22a2650898514f04cef594ff2
lord_0bf38w16110335h@yahoo.co.jp <59830008+issei1213@users.noreply.github.com>
2020-04-21 17:41:47 +0900

    gem kaminari install

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,6 +1,8 @@
 class BoardsController < ApplicationController
+  before_action :set_target_board, only: %i[show edit update destroy]
+
   def index
-    @board = Board.all
+    @boards = Board.page(params[:page])
   end
 
   def new
@@ -8,27 +10,35 @@ class BoardsController < ApplicationController
   end
 
   def create
-    Board.create(board_params)
+    @board = Board.create(board_params)
+
+    redirect_to @board
   end
 
   def show
-    @board = Board.find(params[:id])
   end
 
   def edit
-    @board = Board.find(params[:id])
   end
 
   def update
-    board = Board.find(params[:id])
-    board.update(board_params)
+    @board.update(board_params)
+    redirect_to @board
+  end
 
-    redirect_to board
+  def destroy
+    @board.delete
+
+    redirect_to boards_path
   end
 
   private
 
   def board_params
     params.require(:board).permit(:name, :title, :body)
+  end
+
+  def set_target_board
+    @board = Board.find(params[:id])
   end
 end
